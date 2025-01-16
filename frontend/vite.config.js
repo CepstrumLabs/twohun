66935ec3c155d41303803d771c5aecc72a5d74cd
michael-karotsieris <michael.karotsieris@gmail.com>
2025-01-16 18:42:39 +0000

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
     sourcemap: false,
     minify: 'terser',
     target: 'es2015'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   server: {
     port: 3000,
