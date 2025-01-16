71ab43c8e652eba6af103b1d43f9d48f83cb0630
michael-karotsieris <michael.karotsieris@gmail.com>
2025-01-16 20:40:18 +0000

    switch-apps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,35 @@
-import { useState, useEffect } from 'react';
-import StockListContainer from './containers/StockListContainer.jsx';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [currentDate, setCurrentDate] = useState('');
-
-  useEffect(() => {
-    const date = new Date();
-    const formattedDate = date.toLocaleDateString('en-US', {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
-    setCurrentDate(formattedDate);
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>TwoHun</h1>
-      <h3>{currentDate}</h3>
-      <StockListContainer />
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
