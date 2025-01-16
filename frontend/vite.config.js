5e83c76227c7f8007cc4da979863924a0b13fb5f
michael-karotsieris <michael.karotsieris@gmail.com>
2025-01-16 20:09:03 +0000

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,34 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Explicitly include JSX files
-      include: /\.(jsx|js)$/,
-      // Add Babel options
-      babel: {
-        presets: ['@babel/preset-react'],
-        plugins: [],
-        babelrc: false,
-        configFile: false,
-      },
-    })
-  ],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    },
-  },
-}) 
+  plugins: [react()],
+})
